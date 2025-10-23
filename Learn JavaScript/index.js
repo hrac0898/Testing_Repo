7a4fb744e9a388f9d211ca6368571effd45be1ae
hrac0898 <hrac0898@gmail.com>
2025-10-23 19:54:39 +0800

    Finished Introduction in Javascript

--- Learn JavaScript/index.js
+++ Learn JavaScript/index.js
@@ -1,4 +1,17 @@
 console.log('Hello World');
 
 const myName = "Harold Castro";
+const h1 =document.querySelector(".heading-primary");
 console.log(myName);
+
+//  h1.textContent= myName;
+
+
+h1.addEventListener('click', function(){
+    h1.textContent= myName;
+});
+
+const yearEL=document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEL.textContent= currentYear
